4dddac1c0284cc4802fabbbf22d9cc0ed075f208
Eric Murphy <murph141@purdue.edu>
2014-12-29 21:02:09 -0500

    Changing logic to purely combinational

--- DigitalLogic/Lab1/TestBench.v
+++ DigitalLogic/Lab1/TestBench.v
@@ -4,26 +4,34 @@ module TestBench();
 
   reg [9:0] Switches;
   wire [9:0] Leds;
+  wire [0:6] Disp0, Disp1, Disp2, Disp3, Disp4, Disp5;
 
   initial
   begin
     $dumpfile("Waves.vcd");
     $dumpvars(0, TestBench);
 
-    Switches <= 9'b000000000;
-
+    Switches[9:0] = 9'b0;
     #10
 
-    Switches <= 9'b010101011;
-
+    Switches[9:7] = 3'b001;
     #10
 
-    Switches <= 9'b0zzxx1111;
-
+    Switches[9:7] = 3'b010;
     #10
+
+    Switches[9:7] = 3'b011;
+    #10
+
+    Switches[9:7] = 3'b100;
+    #10
+
+    Switches[9:7] = 3'b101;
+    #10
+
     $finish;
   end
 
-  Lab1 Test(.SW(Switches), .LEDR(Leds));
+  Lab1 Test(.SW(Switches), .LEDR(Leds), .HEX0(Disp0), .HEX1(Disp1), .HEX2(Disp2), .HEX3(Disp3), .HEX4(Disp4), .HEX5(Disp5));
 
 endmodule
