ffe60c7f3f69ad3fc45fdb7295de8f718adc086d
Eric Murphy <murph141@purdue.edu>
2014-12-31 18:20:06 -0500

    Minor fix

--- DigitalLogic/Lab4/Lab4.v
+++ DigitalLogic/Lab4/Lab4.v
@@ -1,34 +1,34 @@
-module Lab4(KEY0, SW, HEX0, HEX1, LEDR);
+module Lab4(KEY, SW, HEX0, HEX1, LEDR);
 
   input [9:0] SW;
-  input KEY0;
+  input [3:0] KEY;
   output [0:6] HEX0, HEX1;
   output [9:0] LEDR;
 
   wire [7:0] Qo, T;
 
-  T_FF F0(.En(SW[1]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[0]));
+  T_FF F0(.En(SW[1]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[0]));
 
   assign T[0] = SW[1] & Qo[0];
-  T_FF F1(.En(T[0]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[1]));
+  T_FF F1(.En(T[0]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[1]));
   
   assign T[1] = T[0] & Qo[1];
-  T_FF F2(.En(T[1]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[2]));
+  T_FF F2(.En(T[1]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[2]));
   
   assign T[2] = T[1] & Qo[2];
-  T_FF F3(.En(T[2]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[3]));
+  T_FF F3(.En(T[2]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[3]));
 
   assign T[3] = T[2] & Qo[3];
-  T_FF F4(.En(T[3]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[4]));
+  T_FF F4(.En(T[3]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[4]));
 
   assign T[4] = T[3] & Qo[4];
-  T_FF F5(.En(T[4]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[5]));
+  T_FF F5(.En(T[4]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[5]));
 
   assign T[5] = T[4] & Qo[5];
-  T_FF F6(.En(T[5]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[6]));
+  T_FF F6(.En(T[5]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[6]));
 
   assign T[6] = T[5] & Qo[6];
-  T_FF F7(.En(T[6]), .Clk(KEY0), .Clr(SW[0]), .Q(Qo[7]));
+  T_FF F7(.En(T[6]), .Clk(KEY[0]), .Clr(SW[0]), .Q(Qo[7]));
 
   Hex_Disp H0(.Value(Qo[3:0]), .Disp(HEX0[0:6]));
   Hex_Disp H1(.Value(Qo[7:4]), .Disp(HEX1[0:6]));
